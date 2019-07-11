e33af604c6dacdfdb5ec9f99accac7b36e8efb78
paphatsiri <paphatsiri.work@gmail.com>
2019-07-11 22:59:10 +0700

    add

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+//npm run Server to run nodemon
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request =  require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.listen(3000, function() {
+        console.log("Server is running on port 3000!");
+});
